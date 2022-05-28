27dea8137da25eee86963f941edd0575db37abd3
chrispobrien <chris.obrien57@gmail.com>
2022-05-28 11:19:17 -0400

    Switch to google books for book page

--- assets/js/book.js
+++ assets/js/book.js
@@ -16,34 +16,66 @@ var localSourceData = {
     book: {}
   };
 
+var insertLineBreaks = function(text) {
+    var postText = "";
+    for (let i=0;i<text.length;i++) {
+        if (text.substring(i,i+1) === '\n' || text.substring(i,i+1) === '\r')
+            postText += '<p>'
+        else
+            postText += text.substring(i,i+1)
+    }
+    return postText;
+}
+
 var showBook = function() {
-    let title = document.createElement("h1");
-    title.textContent = localSourceData.bookResults.books[book].title;
-    mainEl.appendChild(title);
-    let author = document.createElement("h2");
-    author.textContent = 'Author: '+localSourceData.bookResults.books[book].author;
-    mainEl.appendChild(author);
-    let description = document.createElement("p");
-    //description.textContent = localSourceData.bookResults.books[book].description;
-    description.textContent = localSourceData.book.items[0].volumeInfo.description;
-    description.setAttribute("class","mb-5");
-    mainEl.appendChild(description);
-    let buyLink = document.createElement("a");
-    buyLink.setAttribute("href",localSourceData.bookResults.books[book].amazon_product_url);
-    buyLink.setAttribute("class","m-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800");
-    buyLink.textContent = "Buy";
-    mainEl.appendChild(buyLink);
-    let borrowLink = document.createElement("a");
-    let NYPLUrl = "https://browse.nypl.org/iii/encore/search/C__S"
-        + isbn13 + "__Orightresult__U?lang=eng&suite=def";
-    borrowLink.setAttribute("href",NYPLUrl);
-    borrowLink.setAttribute("class","m-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800");
-    borrowLink.textContent = "Borrow";
-    mainEl.appendChild(borrowLink);
-    let viewerEl = document.createElement("div");
-    viewerEl.setAttribute("style","mt-5 width: 600px; height: 500px");
-    viewerEl.setAttribute("id","viewerCanvas");
-    mainEl.appendChild(viewerEl);
+    let title = document.querySelector("#title");
+    title.textContent = localSourceData.book.items[0].volumeInfo.title;
+    let subtitle = document.querySelector("#subtitle");
+    subtitle.textContent = localSourceData.book.items[0].volumeInfo.subtitle
+    let pages = document.querySelector("#pages");
+    pages.textContent = "Pages: "+localSourceData.book.items[0].volumeInfo.pageCount;
+
+    // There can be more than one author
+    // TODO: Style multiple authors, <br /> ?
+    let author = document.querySelector("#author");
+    if (localSourceData.book.items[0].volumeInfo.authors.length>1) {
+        author.textContent = 'Authors: ';
+        for (let i=0;i<localSourceData.book.items[0].volumeInfo.authors.length;i++)
+            author.textContent += localSourceData.book.items[0].volumeInfo.authors[i] + ", ";
+        author.textContent = author.textContent.substring(0,author.textContent.length-2);
+    } else {
+        author.textContent = 'Author: ' + localSourceData.book.items[0].volumeInfo.authors[0]
+    }
+
+    let description = document.querySelector("#description");
+    description.innerHTML = "";
+    let coverDiv = document.createElement("div");
+    coverDiv.setAttribute("class","float-right p-2");
+    let cover = document.createElement("img");
+    cover.setAttribute("src",localSourceData.book.items[0].volumeInfo.imageLinks.smallThumbnail.replace("http","https"));
+    cover.setAttribute("alt","Book Cover");
+    coverDiv.appendChild(cover);
+    description.appendChild(coverDiv);
+    let textDiv = document.createElement("div");
+    textDiv.textContent = insertLineBreaks(localSourceData.book.items[0].volumeInfo.description);
+    description.appendChild(textDiv);
+
+    // let buyLink = document.createElement("a");
+    // buyLink.setAttribute("href",localSourceData.bookResults.books[book].amazon_product_url);
+    // buyLink.setAttribute("class","m-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800");
+    // buyLink.textContent = "Buy";
+    // mainEl.appendChild(buyLink);
+    // let borrowLink = document.createElement("a");
+    // let NYPLUrl = "https://browse.nypl.org/iii/encore/search/C__S"
+    //     + isbn13 + "__Orightresult__U?lang=eng&suite=def";
+    // borrowLink.setAttribute("href",NYPLUrl);
+    // borrowLink.setAttribute("class","m-2 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800");
+    // borrowLink.textContent = "Borrow";
+    // mainEl.appendChild(borrowLink);
+    // let viewerEl = document.createElement("div");
+    // viewerEl.setAttribute("style","mt-5 width: 600px; height: 500px");
+    // viewerEl.setAttribute("id","viewerCanvas");
+    // mainEl.appendChild(viewerEl);
 };
 
 var initViewer = function() {
@@ -78,8 +110,8 @@ var getBook = function() {
           response.json().then(function(data) {
             localSourceData.book = data;
             saveLocalSourceData();
-            // Now that we have a list of books, show them
-            //populateBooks();
+            book = localSourceData.bookResults.books.findIndex(books => books.primary_isbn13 === isbn13);
+            showBook();
           })
         }
       })
@@ -92,18 +124,21 @@ var getBook = function() {
 
 // So this localStorage should be filled whenever user browses to book page
 var loadLocalSourceData = function() {
-    google.books.load();
-    google.books.setOnLoadCallback(initViewer);
+    //google.books.load();
+    //google.books.setOnLoadCallback(initViewer);
     let lsd = localStorage.getItem("nyt");
     if (lsd) {
       localSourceData = JSON.parse(lsd);
     };
     if (!lsd.book || lsd.book.items[0].volumeInfo.industryIdentifiers[0].identifier != isbn13) {
         getBook();
+    } else {
+        book = localSourceData.bookResults.books.findIndex(books => books.primary_isbn13 === isbn13);
+        showBook();
     }
     book = localSourceData.bookResults.books.findIndex(books => books.primary_isbn13 === isbn13);
     showBook();
-    addGoogleBooks();
+    //addGoogleBooks();
 };
 
 var saveLocalSourceData = function() {
