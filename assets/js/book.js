fc087487e009775ff2b1e0423cb25459957a38bf
chrispobrien <chris.obrien57@gmail.com>
2022-05-30 14:12:49 -0400

    Added reviews

--- assets/js/book.js
+++ assets/js/book.js
@@ -1,7 +1,10 @@
+var nytAPIKey = 'DRc2qz1HkMLbgp0o1kkGqcDcAG1cTfg6';
 var mainEl = document.querySelector("#main");
+// book.html must be passed isbn13 parameter to identify the book to show
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 const isbn13 = urlParams.get('isbn13');
+// Book index within bestseller list
 var book = 0;
 
 // Template of local object to store info
@@ -13,20 +16,12 @@ var localSourceData = {
     selected: 0,
     date: '',
     // Book data
-    book: {}
+    book: {},
+    // NY Times reviews, if any
+    reviews: []
   };
 
-var insertLineBreaks = function(text) {
-    var postText = "";
-    for (let i=0;i<text.length;i++) {
-        if (text.substring(i,i+1) === '\n' || text.substring(i,i+1) === '\r')
-            postText += '<p>'
-        else
-            postText += text.substring(i,i+1)
-    }
-    return postText;
-}
-
+// Once book is loaded, show details  
 var showBook = function() {
     let title = document.querySelector("#title");
     title.textContent = localSourceData.book.items[0].volumeInfo.title;
@@ -35,8 +30,7 @@ var showBook = function() {
     let pages = document.querySelector("#pages");
     pages.textContent = "Pages: "+localSourceData.book.items[0].volumeInfo.pageCount;
 
-    // There can be more than one author
-    // TODO: Style multiple authors, <br /> ?
+    // There can be more than one author, so create divs for the label and each author
     let author = document.querySelector("#author");
     author.innerHTML = "";
     let label = document.createElement("div");
@@ -55,11 +49,12 @@ var showBook = function() {
         authors.appendChild(newAuthor);
     }
         
-    
     authors.setAttribute("class","grid grid-cols-1 float-right mx-auto");
 
     author.appendChild(label);
     author.appendChild(authors);
+
+    // Proceeding with loading the rest of the page
     let description = document.querySelector("#description");
     description.innerHTML = "";
     let coverDiv = document.createElement("div");
@@ -70,9 +65,10 @@ var showBook = function() {
     coverDiv.appendChild(cover);
     description.appendChild(coverDiv);
     let textDiv = document.createElement("div");
-    textDiv.textContent = insertLineBreaks(localSourceData.book.items[0].volumeInfo.description);
+    textDiv.textContent = localSourceData.book.items[0].volumeInfo.description;
     description.appendChild(textDiv);
 
+    // Create buy links from NY Times buy links list
     let buyLinksEl = document.querySelector("#buyLinks");
     buyLinksEl.innerHTML = "";
     let buyTitle = document.createElement("div");
@@ -90,6 +86,7 @@ var showBook = function() {
         buyLinksEl.appendChild(buyLinkDiv);
     }
 
+    // Create borrow link, NY Public Library
     let borrowLinksEl = document.querySelector("#borrowLinks");
     borrowLinksEl.innerHTML = "";
     let borrowTitle = document.createElement("div");
@@ -100,14 +97,14 @@ var showBook = function() {
     let borrowLinkDiv = document.createElement("div");
     borrowLinkDiv.setAttribute("class","mb-5");
     let borrowLink = document.createElement("a");
-    borrowLink.setAttribute("href","https://browse.nypl.org/iii/encore/search/C__S"
-         + isbn13 + "__Orightresult__U?lang=eng&suite=def");
+    borrowLink.setAttribute("href",`https://browse.nypl.org/iii/encore/search/C__S${isbn13}__Orightresult__U?lang=eng&suite=def`);
     borrowLink.setAttribute("class","bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4");
     borrowLink.textContent = "NY Public Library";
     borrowLinkDiv.appendChild(borrowLink)
     borrowLinksEl.appendChild(borrowLinkDiv);
 };
 
+// This code is not used at the moment, it is to load a preview from Google
 var initViewer = function() {
     let viewer = new google.books.DefaultViewer(document.querySelector("#viewerCanvas"));
     viewer.load('ISBN:'+isbn13, console.log("Book not found"));
@@ -115,8 +112,7 @@ var initViewer = function() {
 
 // Inserts a script function to call Google Books API - callback function handleGBResponse
 var addGoogleBooks = function() {
-    let gbUrl = "https://books.google.com/books?jscmd=viewapi&bibkeys=ISBN:"
-        + isbn13 + "&callback=handleGBResponse";
+    let gbUrl = `https://books.google.com/books?jscmd=viewapi&bibkeys=ISBN:${isb13}&callback=handleGBResponse`
     let gbEl = document.createElement("script");
     gbEl.setAttribute("src",gbUrl);
     gbEl.setAttribute("type","text/javascript");
@@ -128,10 +124,57 @@ var handleGBResponse = function(data) {
     console.log(data);
 }
 
+// Show reviews if any
+var showReviews = function() {
+    let reviewsEl = document.querySelector("#reviews");
+    reviewsEl.innerHTML = "";
+    if (localSourceData.reviews.num_results) {
+        let reviewTitle = document.createElement("div");
+        reviewTitle.textContent = "Reviews";
+        reviewTitle.setAttribute("class","mx-auto border-t-4 border-slate-800");
+        reviewsEl.appendChild(reviewTitle);
+        for (i=0;i<localSourceData.reviews.num_results;i++) {
+            let newReview = document.createElement("div");
+            newReview.setAttribute("class","");
+            newReview.textContent = localSourceData.reviews.results[i].byline + ': '
+                + localSourceData.reviews.results[i].summary;
+            let newLinkWrapper = document.createElement("div");
+            newLinkWrapper.setAttribute("class","mx-auto py-2");
+            let newLink = document.createElement("a");
+            newLink.setAttribute("href",localSourceData.reviews.results[i].url);
+            newLink.setAttribute("class","bg-slate-500 hover:bg-slate-700 text-white font-bold py-2 px-4");
+            newLink.textContent = "Review";
+            newLinkWrapper.appendChild(newLink);
+            newReview.appendChild(newLinkWrapper);
+            reviewsEl.appendChild(newReview);
+        }
+    }
+}
+
+// Get review(s) from NY Times for this book
+var getReviews = function() {
+    let Url = `https://api.nytimes.com/svc/books/v3/reviews.json?isbn=${isbn13}&api-key=${nytAPIKey}`;
+
+    fetch(Url)
+        .then(function(response) {
+            if (response.ok) {
+                response.json().then(function(data) {
+                    localSourceData.reviews = data;
+                    saveLocalSourceData();
+                    showReviews();
+                })
+            } else {
+                localSourceData.reviews = {};
+            }
+        })
+        .catch(function(error) {
+            localSourceData.reviews = {};
+        })
+}
+
 // Google Books API, gathers more info on this book
 var getBook = function() {
-    let Url = 'https://www.googleapis.com/books/v1/volumes?q=isbn:'
-    + isbn13;
+    let Url = `https://www.googleapis.com/books/v1/volumes?q=isbn:${isbn13}`
   
     fetch(Url)
       .then(function(response) {
@@ -161,12 +204,14 @@ var loadLocalSourceData = function() {
     };
     if (!lsd.book || lsd.book.items[0].volumeInfo.industryIdentifiers[0].identifier != isbn13) {
         getBook();
+        getReviews();
     } else {
+        // Find and set book index within Bestseller list array by isbn13 number
         book = localSourceData.bookResults.books.findIndex(books => books.primary_isbn13 === isbn13);
         showBook();
     }
     book = localSourceData.bookResults.books.findIndex(books => books.primary_isbn13 === isbn13);
-    showBook();
+    //showBook();
     //addGoogleBooks();
 };
 
